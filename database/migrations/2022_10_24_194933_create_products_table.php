49005645b66887413b467d915eb310459ca116ea
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-10-24 17:45:14 -0300

    tabela de produtos

---
+++ database/migrations/2022_10_24_194933_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('name')->unique();
+            $table->string('image')->nullable();
+            $table->integer('quantity_inventory')->require();
+            $table->text('description')->nullable();
+            $table->float('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
