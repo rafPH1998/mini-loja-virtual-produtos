38517ddc5ae1393bd120cc7a32768f2e32b60563
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-11-01 14:25:52 -0300

    Routes resource

--- routes/web.php
+++ routes/web.php
@@ -6,22 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::resource('/products', ProductController::class)
     ->middleware('auth');
 
-// Route::get('/products', function () {
-//     return view('indexProduct');
-// })->middleware(['auth']);
-
-
-// Route::get('/add', function () {
-//     return view('add');
-// });
-
-// Route::get('/show', function () {
-//     return view('show');
-// });
-
-// Route::get('/product', function () {
-//     return view('product');
-// });
-
-
 require __DIR__.'/auth.php';
