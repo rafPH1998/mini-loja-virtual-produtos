ae72e028825c0a2a461a9fea6162f9bbda49bbb5
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-11-01 14:24:45 -0300

    Gate update-product

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -25,6 +27,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('update-product', function (User $user, Product $product) {
+            return $user->id === $product->user_id;
+        });
     }
 }
