7e636ad2621e7bb732c7a4ba96c706f20fa606de
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-11-01 14:23:40 -0300

    Paginate, with, create e gate

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,15 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Products\StoreAndUpdateProduct;
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class ProductController extends Controller
 {
 
+    public function __construct(
+        protected User $user, 
+        protected Product $product)
+    { }
+
     public function index()
     {
-        $products = Product::paginate(4);
+        $products = $this->product
+                    ->with('user')
+                    ->paginate(5);
 
         return view('products.index', [
             'products' => $products
@@ -19,22 +29,36 @@ class ProductController extends Controller
 
     public function create()
     {
-        //
+        return view('products.add');
     }
 
-    public function store(Request $request)
+    public function store(StoreAndUpdateProduct $request)
     {
-        //
+        
+        $product = auth()->user()
+                ->products()
+                ->create($request->validated());
+
+        return redirect()->route('products.index')
+                        ->with('success', "Produto {$product->name} criado!");
     }
 
     public function show($id)
     {
-        //
+        $product = $this->product->findOrFail($id);
+        return view('products.show', compact('product'));
     }
 
     public function edit($id)
     {
-        //
+        $product = $this->product->find($id);
+
+        if (!Gate::authorize('update-product', $product)) {
+            return false;
+        }
+
+        return view('products.show', compact('product'));
+
     }
 
     public function update(Request $request, $id)
