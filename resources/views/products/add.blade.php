da6fe1b7f8bcaed881a5ec409e830b03227cf4b2
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-11-01 14:25:20 -0300

    View para adicoinar um produto

--- resources/views/products/add.blade.php
+++ resources/views/products/add.blade.php
@@ -3,17 +3,26 @@
 @section('title', 'Adicionar um produto novo')
 
 @section('content')
-    <div class="lg:w-2/4 w-full mx-auto overflow-auto">
-        <div class="flex items-center justify-between mb-2">
+    @include('components-alerts/alerts')
+
+    <div class="lg:w-2/4 w-full mx-auto overflow-auto shadow-2xl rounded-md">
+        <div class="flex items-center justify-between mb-2 px-5 py-5">
             <h1 class="text-2xl font-medium title-font mb-2 text-gray-900">Adicionar produto</h1>
         </div>
-
-        <form>
+    
+        <form method="POST" class="px-10 py-10" action="{{ route('products.store') }}">
             <div class="flex flex-wrap">
+                @csrf
                 <div class="p-2 w-1/2">
                     <div class="relative">
                         <label for="name" class="leading-7 text-sm text-gray-600">Nome do produto</label>
-                        <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                        <input type="text" id="name" name="name" 
+                                value="{{ old('name') }}" 
+                                class="w-full bg-gray-100 bg-opacity-50 
+                                rounded border border-gray-300 focus:border-indigo-500 
+                                focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base 
+                                outline-none text-gray-700 py-1 px-3 leading-8 
+                                transition-colors duration-200 ease-in-out">
                     </div>
                 </div>
 
@@ -21,37 +30,68 @@
                     <div class="relative">
                         <label for="name" class="leading-7 text-sm text-gray-600">Preço</label>
                         <input type="text" id="price" name="price"
-                            class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                            value="{{ old('price') }}"
+                            class="w-full bg-gray-100 bg-opacity-50 rounded border 
+                            border-gray-300 focus:border-indigo-500 focus:bg-white 
+                            focus:ring-2 focus:ring-indigo-200 text-base outline-none 
+                            text-gray-700 py-1 px-3 leading-8 transition-colors 
+                            duration-200 ease-in-out" />
                     </div>
                 </div>
 
                 <div class="p-2 w-1/2">
                     <div class="relative">
                         <label for="name" class="leading-7 text-sm text-gray-600">Estoque</label>
-                        <input type="text" id="stock" name="stock"
-                            class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                        <input type="text" id="quantity_inventory" name="quantity_inventory"
+                            value="{{ old('quantity_inventory') }}"
+                            class="w-full bg-gray-100 bg-opacity-50 rounded 
+                            border border-gray-300 focus:border-indigo-500 
+                            focus:bg-white focus:ring-2 focus:ring-indigo-200 
+                            text-base outline-none text-gray-700 py-1 px-3 
+                            leading-8 transition-colors duration-200 ease-in-out">
                     </div>
                 </div>
 
                 <div class="p-2 w-1/2">
                     <div class="relative">
                         <label for="name" class="leading-7 text-sm text-gray-600">Imagem de capa</label>
-                        <input type="file" id="cover" name="cover"
-                            class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        <input type="file" id="image" name="image"
+                            value="{{ old('image') }}"
+                            class="w-full bg-gray-100 bg-opacity-50 
+                            rounded border border-gray-300 
+                            focus:border-indigo-500 focus:bg-white 
+                            focus:ring-2 focus:ring-indigo-200 text-base 
+                            outline-none text-gray-700 py-1 px-3 leading-8 
+                            transition-colors duration-200 ease-in-out" />
                     </div>
                 </div>
 
                 <div class="p-2 w-full">
                     <div class="relative">
-                        <label for="name" class="leading-7 text-sm text-gray-600">Descrição</label>
+                        <label for="name" 
+                        class="leading-7 text-sm text-gray-600">Descrição</label>
                         <textarea
                             id="description" name="description"
-                            class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></textarea>
+                            class="w-full bg-gray-100 bg-opacity-50 
+                            rounded border border-gray-300 
+                            focus:border-indigo-500 focus:bg-white 
+                            focus:ring-2 focus:ring-indigo-200 
+                            text-base outline-none text-gray-700 
+                            py-1 px-3 leading-8 transition-colors 
+                            duration-200 ease-in-out">
+                            {{ old('description') }}
+                        </textarea>
                     </div>
                 </div>
 
                 <div class="p-2 w-full">
-                    <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Adicionar</button>
+                    <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 
+                            focus:ring-blue-300 font-medium rounded-lg 
+                            text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 
+                            dark:hover:bg-blue-700 focus:outline-none 
+                            dark:focus:ring-blue-800">
+                            Adicionar
+                    </button>
                 </div>
 
             </div>
