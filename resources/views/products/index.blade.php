50a822d4e142bc2030cadc4c689cec5a5bf6324e
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-11-02 21:02:42 -0300

    form para filtros

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,156 +4,148 @@
 
 @section('content')
 
-    <div class="lg:w-2/3 w-full mx-auto overflow-auto flex items-center justify-between mb-2">
-        <h1 class="text-2xl font-medium title-font mb-2 text-gray-900">Produtos</h1>
+    <div class="mb-5 ml-5">
         <a href="{{ route('products.create') }}" 
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 
             focus:ring-blue-300 font-medium rounded-lg 
-            text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 
+            text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600  
             dark:hover:bg-blue-700 focus:outline-none 
             dark:focus:ring-blue-800">
             Adicionar
         </a>
     </div>
 
-    <div class="lg:w-2/3 w-full mx-auto overflow-auto shadow-xl rounded-md mt-5">
-        
-        @include('components-alerts/alerts')
-        
-        <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
-            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" class="py-3 px-6">
-                            #
-                        </th>
-                        <th scope="col" class="py-3 px-6">
-                            Imagem
-                        </th>
-                        <th scope="col" class="py-3 px-6">
-                            Nome Produto
-                        </th>
-                        <th scope="col" class="py-3 px-6">
-                            Valor
-                        </th>
-                        <th scope="col" class="py-3 px-6">
-                            Estoque
-                        </th>
-                        <th scope="col" class="py-3 px-6">
-                            Ações
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-			        @foreach ($products as $product )
-                    
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <td class="py-4 px-6">
-                                {{ $product->id }}
-                            </td>
-                            <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Sem imagem
-                            </th>
-                            <td class="py-4 px-6">
-                                {{ $product->name }}
-                            </td>
-                            <td class="py-4 px-6">
-                                $ {{ number_format($product->price , 2, ',', '.') }}
-                            </td>
-                            <td class="py-4 px-6">
-                                {{ $product->quantity_inventory > 0 ? $product->quantity_inventory : 'Sem estoque'}}
-                            </td>
-                            <td class="py-4 px-6">
-                                <a href="{{ route('products.show', $product->id) }}" 
-                                    class="focus:outline-none text-white bg-yellow-400 
-                                    hover:bg-yellow-500 focus:ring-4 
-                                    focus:ring-yellow-300 font-medium rounded-lg 
-                                    text-xs px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">
-                                    Ver
-                                </a>
-                                @can('update-product', $product)
-                                    <a href="{{ route('products.edit', $product->id) }}" 
-                                        class="focus:outline-none text-white bg-purple-700 
-                                        hover:bg-purple-800 focus:ring-4 focus:ring-purple-300
-                                        font-medium rounded-lg text-xs px-5 py-2.5 mb-2 
-                                        dark:bg-purple-600 dark:hover:bg-purple-700 
-                                        dark:focus:ring-purple-900">
-                                        Editar
-                                    </a>
-                                    <a href="" 
-                                        class="ml-2 focus:outline-none text-white bg-red-700 
-                                        hover:bg-red-800 focus:ring-4 
-                                        focus:ring-red-300 font-medium rounded-lg 
-                                        text-xs px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 
-                                        dark:hover:bg-red-700 dark:focus:ring-red-900">
-                                        Deletar
-                                    </a>
-                                @endcan     
+    <div class="ml-5 mt-5 flex">
+        <div class="md:flex-nowrap flex-wrap justify-center md:justify-start">
 
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-
-            <nav class="flex justify-between items-center pt-4" aria-label="Table navigation">
-                <ul class="inline-flex items-center -space-x-px py-2.5 px-2.5">
-                    @if ($products->currentPage() > 1)
-                        <li>
-                            <a href="?page={{ $products->currentPage() - 1 }}" class="block py-2 px-3 ml-0 leading-tight text-gray-500 bg-white 
-                                rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 
-                                dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                                <span class="sr-only">Previous</span>
-                                <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                            </a>
-                        </li>
-                    @endif
-                    <li>
-                        <a href="#" aria-current="page" 
-                            class="py-2 px-3 text-blue-600 bg-blue-50 border 
-                            border-gray-300 hover:bg-blue-100 hover:text-blue-700 
-                            dark:border-gray-700 dark:bg-gray-700 dark:text-white">
-                            {{ $products->currentPage() }}
-                        </a>
-                    </li>
-                    @if ($products->currentPage() < $products->lastPage())
-                        <li>
-                            <a href="?page={{ $products->currentPage() + 1 }}" class="block py-2 px-3 leading-tight text-gray-500 bg-white 
-                                rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 
-                                dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 
-                                dark:hover:text-white">
-                                <span class="sr-only">Next</span>
-                                    <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" 
-                                        xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" 
-                                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 
-                                        1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd">
-                                    </path>
-                                </svg>
-                            </a>
-                        </li>
-                    @endif
-                </ul>
-            </nav>
-        </div>
-    </div>
-
-    <div class="lg:w-2/3 w-full mx-auto overflow-auto mt-5">
-        <div class="flex md:flex-nowrap flex-wrap justify-center md:justify-start">
-            <form action="#" method="GET">
-                <input type="text" id="footer-field" 
-                    placeholder="Procurar um produto" 
-                    name="search" 
-                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 
-                    focus:ring-2 focus:bg-transparent focus:ring-indigo-200 
-                    focus:border-indigo-500 text-base outline-none text-gray-700 
-                    py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-
-                <button class="mt-1 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Procurar</button>
+            <form method="GET" action="#" class="flex items-center">   
+                <label for="simple-search" class="sr-only">Search</label>
+                <div class="relative w-full">
+                    <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" 
+                            fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817
+                                 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd">
+                            </path>
+                        </svg>
+                    </div>
+                    <input type="text" name="filter"
+                            class="bg-gray-50 border border-gray-300 
+                            text-gray-900 text-sm rounded-lg focus:ring-blue-500 
+                            focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 
+                            dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                            dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Procure por um produto"
+                    >
+                </div>
             </form>
         </div>
+        
+        <form action="#" method="GET" name="formSelect" class="ml-5">
+            <div class="max-w-2xl flex">
+                <select id="status" name="status" 
+                        class="bg-gray-50 border border-gray-300 text-gray-900
+                        text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full
+                        p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                        dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option value="cheap">Produtos mais baratos</option>
+                        <option value="expensive">Produtos mais caros</option>
+                        <option value="last_registered">Últimos cadastrados</option>
+                </select>
+
+                <button type="submit" 
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 
+                    focus:ring-blue-300 font-medium rounded-lg 
+                    text-sm px-5 py-2 px-4 dark:bg-blue-600  
+                    dark:hover:bg-blue-700 focus:outline-none 
+                    dark:focus:ring-blue-800">    
+                    Filtrar
+                </button>
+            </div>
+        </form>
+
     </div>
 
+    <div class="flex items-stretch drop-shadow-xl">
+        @forelse ($products as $product)
+            <div class="w-96 bg-white rounded-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700 ml-4 mt-5">
+                <a href="#">
+                    <img class="rounded-t-lg" src="{{ url('images/laravel.svg') }}" alt="" />
+                </a>
+                <div class="p-5" >
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        <b>{{ $product->name }}</b>
+                    </p>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        {{ $product->description }}
+                    </p>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        $ {{ number_format($product->price , 2, ',', '.') }}
+                    </p>
+                    <p class="mb-1 text-sm mt-7 dark:text-gray-400">
+                        Criado em: {{ $product->created_at }}
+                    </p>
+                    <a href="{{ route('products.show', $product->id) }}" class="mt-3 text-indigo-500 inline-flex items-center">Ver mais
+                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                            <path d="M5 12h14M12 5l7 7-7 7"></path>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        @empty
+            <p class="ml-5 mt-5 mt-5">
+                Nenhum produto encontrado.
+            </p>
+        @endforelse
+    </div>
 
+    @if ($products->total() > 4)
+        <nav class="flex justify-between items-center pt-4" aria-label="Table navigation">
+            <ul class="inline-flex items-center -space-x-px py-2.5 px-2.5">
+                @if ($products->currentPage() > 1)
+                    <li>
+                        <a href="?page={{ $products->currentPage() - 1 }}" class="block py-2 px-3 ml-0 leading-tight text-gray-500 bg-white 
+                            rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 
+                            dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                            <span class="sr-only">Previous</span>
+                            <svg class="w-5 h-5" aria-hidden="true" 
+                                fill="currentColor" viewBox="0 0 20 20" 
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" 
+                                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 
+                                    1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd">
+                                </path>
+                            </svg>
+                        </a>
+                    </li>
+                @endif
+                <li>
+                    <a href="#" aria-current="page" 
+                        class="py-2 px-3 text-blue-600 bg-blue-50 border 
+                        border-gray-300 hover:bg-blue-100 hover:text-blue-700 
+                        dark:border-gray-700 dark:bg-gray-700 dark:text-white">
+                        {{ $products->currentPage() }}
+                    </a>
+                </li>
+                @if ($products->currentPage() < $products->lastPage())
+                    <li>
+                        <a href="?page={{ $products->currentPage() + 1 }}" class="block py-2 px-3 leading-tight text-gray-500 bg-white 
+                            rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 
+                            dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 
+                            dark:hover:text-white">
+                            <span class="sr-only">Next</span>
+                                <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" 
+                                    xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" 
+                                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 
+                                    1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd">
+                                </path>
+                            </svg>
+                        </a>
+                    </li>
+                @endif
+            </ul>
+        </nav>
+    @endif
 @endsection
 
 
