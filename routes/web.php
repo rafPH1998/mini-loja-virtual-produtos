57ed7b93b6b2aceb912dce7126dcb2b614b410f5
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-11-02 21:03:18 -0300

    rota de meus produtos

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::resource('/products', ProductController::class)
     ->middleware('auth');
 
+Route::get('myProducts', [ProductController::class, 'myProducts'])->name('products.myProducts');   
+
 require __DIR__.'/auth.php';
