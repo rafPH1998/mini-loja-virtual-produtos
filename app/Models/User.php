7d31e7638a8bb2de10f8fb7839e3b9b673e1faaf
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-11-02 21:02:22 -0300

    muttator para nome

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -49,6 +49,12 @@ class User extends Authenticatable
         return $this->hasMany(Product::class);
     }
 
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => ucwords($value),
+        );
+    }
 
     
 }
