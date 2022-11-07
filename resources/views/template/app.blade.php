7d28a73b4ecd4aa32ab9fb7954b103d413534cd4
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-11-07 18:14:37 -0300

    logout

--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -35,27 +35,20 @@
                 <b>Bem vindo</b>, {{ auth()->user()->name }}
             </p>
 
-            <form action="{{route('logout')}}" method="POST">
+            <form action="{{route('logout')}}" method="POST" class="ml-9">
                 @csrf
-                <a href="{{route('logout')}}" 
-                    class="ml-4 text-gray-900 bg-white border border-gray-300 focus:outline-none 
-                        hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm 
-                        px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 
-                        dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 
-                        dark:hover:border-gray-600 dark:focus:ring-gray-700" 
+                <a href="{{route('logout')}}"
                     onclick="event.preventDefault(); this.closest('form').submit();">
-                    Sair
+                    <img style="width:25px;" src="{{ url('images/logout.svg') }}" title="Sair" />
                 </a>
             </form>
 
-            <a href="{{ route('products.index') }}" 
-                class="mt-2 py-2.5 px-5 mr-2 mb-2 text-sm font-medium 
-                text-gray-900 focus:outline-none bg-white rounded-lg 
-                border border-gray-200 hover:bg-gray-100 hover:text-blue-700 
-                focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 
-                dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 
-                dark:hover:text-white dark:hover:bg-gray-700">
-                Home
+            <a href="{{ route('products.index') }}" class="ml-2">
+                <img style="width:25px;" src="{{ url('images/home.svg') }}" title="Home"/>
+            </a>
+
+            <a href="{{ route('products.myProducts') }}" class="ml-2">
+                <img style="width:25px;" src="{{ url('images/cart.svg') }}" title="Meus produtos"/>
             </a>
 
         </div>
@@ -95,6 +88,8 @@
             </div>
         </div>
     </footer>
+
+    <script src="{{ url('js/script.js') }}"></script>
 </body>
 </html>
 
