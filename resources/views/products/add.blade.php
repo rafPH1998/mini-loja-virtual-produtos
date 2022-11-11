d4085e0f5e1e895e2899d61b79e0751f4abaece4
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-11-11 15:33:46 -0300

    Exibindo erros na view index

--- resources/views/products/add.blade.php
+++ resources/views/products/add.blade.php
@@ -9,7 +9,7 @@
             <h1 class="text-2xl font-medium title-font mb-2 text-gray-900">Adicionar produto</h1>
         </div>
     
-        <form method="POST" class="px-10 py-10" action="{{ route('products.store') }}">
+        <form method="POST" class="px-10 py-10" action="{{ route('products.store') }}" enctype="multipart/form-data">
             <div class="flex flex-wrap">
                 @csrf
                 <div class="p-2 w-1/2">
@@ -40,6 +40,7 @@
                             focus:ring-2 focus:ring-indigo-200 text-base outline-none 
                             text-gray-700 py-1 px-3 leading-8 transition-colors 
                             duration-200 ease-in-out" />
+                            
                     </div>
                     @error('price')
                         @foreach ($errors->messages()['price'] as $error)
@@ -68,18 +69,41 @@
 
                 <div class="p-2 w-1/2">
                     <div class="relative">
-                        <label for="name" class="leading-7 text-sm text-gray-600">Imagem de capa</label>
+                        <label for="image" class="leading-7 text-sm text-gray-600">Imagem do produto</label>
                         <input type="file" id="image" name="image"
-                            value="{{ old('image') }}"
+                            {{-- value="{{ old('image') }}" --}}
                             class="w-full bg-gray-100 bg-opacity-50 
                             rounded border border-gray-300 
                             focus:border-indigo-500 focus:bg-white 
                             focus:ring-2 focus:ring-indigo-200 text-base 
                             outline-none text-gray-700 py-1 px-3 leading-8 
-                            transition-colors duration-200 ease-in-out" />
+                            transition-colors duration-200 ease-in-out" >
                     </div>
                     @error('image')
                         @foreach ($errors->messages()['image'] as $error)
+                            <span class="text-red-500">{{ $error }}</span>
+                        @endforeach
+                    @enderror
+                </div>
+
+                <div class="p-2 w-1/2">
+                    <div class="relative">
+                        <label for="quality" class="leading-7 text-sm text-gray-600">Estado do produto</label>
+                        <select id="quality" name="quality" 
+                                class="bg-gray-50 border border-gray-300 text-gray-900
+                                text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full
+                                p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                                dark:focus:ring-blue-500 dark:focus:border-blue-500"> 
+                                <option value="">Selecione um estado do produto</option>
+                                @foreach ($qualityStatus as $status)
+                                    <option value="{{$status->name}}">
+                                        {{ $status->value }}
+                                    </option>
+                                @endforeach                      
+                        </select>
+                    </div>
+                    @error('quality')
+                        @foreach ($errors->messages()['quality'] as $error)
                             <span class="text-red-500">{{ $error }}</span>
                         @endforeach
                     @enderror
