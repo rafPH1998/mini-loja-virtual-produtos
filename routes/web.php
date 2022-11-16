69a1ce28044cc4959dfcf7eda5c616184aacadf2
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-11-16 14:31:27 -0300

    Rota para comments

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,21 @@
 
 use App\Http\Controllers\NotifyProductController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
  
 Route::middleware('auth')->group(function() {
+    /**
+    * Route ProductController
+    */
     Route::resource('/products', ProductController::class);
-    
-    Route::get('myProducts', [ProductController::class, 'myProducts'])
-            ->name('products.myProducts');  
+    Route::get('myProducts', [ProductController::class, 'myProducts'])->name('products.myProducts');  
 
-    Route::get('notify', [NotifyProductController::class, 'notify'])
-        ->name('products.notify');
+    /**
+    * Route CommentController
+    */
+    Route::post('/comment', CommentController::class)->name('products.create_comment');
+    Route::get('comments/{comment}', [CommentController::class, 'comments'])->name('products.comments');
 });
 
 
