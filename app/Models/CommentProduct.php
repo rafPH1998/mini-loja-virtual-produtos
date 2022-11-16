4e1f53570bb44fb0eb3f1df90b145686d5424096
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-11-16 14:28:46 -0300

    muttator createdAt e Notifiable

--- app/Models/CommentProduct.php
+++ app/Models/CommentProduct.php
@@ -2,12 +2,29 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class CommentProduct extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected function createdAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Carbon::make($value)->format('d/m/Y')
+        );
+    }
+
 }
