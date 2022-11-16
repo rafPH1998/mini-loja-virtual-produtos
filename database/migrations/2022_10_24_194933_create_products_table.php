ff18462f366fa87ec26bb9c1f2749a4c0b44ca42
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-11-16 14:30:00 -0300

    colocando onDelete('CASCADE')

--- database/migrations/2022_10_24_194933_create_products_table.php
+++ database/migrations/2022_10_24_194933_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('user_id')->constrained('users')->onDelete('CASCADE');
             $table->string('name')->unique();
             $table->string('image');
             $table->integer('quantity_inventory')->require();
