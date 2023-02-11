ea8ba20551fb514616bf1149a3eeb9921e433fe9
Rafael Belchior <rafaelb89.rb@gmail.com>
2023-02-10 22:46:15 -0300

    refatoração no ajax para cadastro de produto

--- public/js/addProduct.js
+++ public/js/addProduct.js
@@ -1,116 +1,88 @@
-document.getElementById("addForm").addEventListener("submit", async function(event){
-    event.preventDefault()
+const form = document.getElementById("addForm");
+const submitButton = document.getElementById("button");
+const nameInput = document.getElementById('name');
+const priceInput = document.getElementById('price');
+const quantityInventoryInput = document.getElementById('quantity_inventory');
+const qualityInput = document.getElementById('quality');
+const typeInput = document.getElementById('type');
+const descriptionInput = document.getElementById('description');
+const tokenInput = document.getElementById('_token');
+const nameErrorSpan = document.getElementById('nameErro');
+const priceErrorSpan = document.getElementById('priceErro');
+const inventoryErrorSpan = document.getElementById('inventoryErro');
+const qualityErrorSpan = document.getElementById('qualityErro');
+const typeErrorSpan = document.getElementById('typeErro');
+const descErrorSpan = document.getElementById('descErro');
+const errorSpans = [nameErrorSpan, priceErrorSpan, inventoryErrorSpan, qualityErrorSpan, typeErrorSpan, descErrorSpan];
+
+form.addEventListener("submit", async function(event) {
+
+    event.preventDefault();
+
+    submitButton.innerHTML = 'Enviando...';
+    submitButton.disabled = true;
+
+    const url = 'http://localhost:8989/products';
     
-    document.getElementById("button").innerHTML = 'Enviando...';
-    document.getElementById("button").disabled = true;
+    try {
+        const response = await fetch(url, {
 
-    /*
-    Valores do formulario
-    */
-    let name               = document.getElementById('name').value;
-    let price              = document.getElementById('price').value;
-    let quantity_inventory = document.getElementById('quantity_inventory').value;
-    let quality            = document.getElementById('quality').value;
-    let type               = document.getElementById('type').value;
-    let description        = document.getElementById('description').value;
-    let token              = document.getElementById('_token').value;
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'accept': 'application/json'
+            },
+            body: JSON.stringify({
+                name:               nameInput.value,
+                price:              priceInput.value,
+                quantity_inventory: quantityInventoryInput.value,
+                quality:            qualityInput.value,
+                type:               typeInput.value,
+                description:        descriptionInput.value,
+                _token:             tokenInput.value
+            })
+        });
 
-    /*
-    Dados do span para exibir o erro
-    */
-    let camposInputs = [
-        document.getElementById('nameErro'),
-        document.getElementById('priceErro'),
-        document.getElementById('inventoryErro'),
-        document.getElementById('qualityErro'),
-        document.getElementById('typeErro'),
-        document.getElementById('descErro'),
-    ];
+        const result = await response.json();
 
-    let url = 'http://localhost:8989/products';
-
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'accept': 'application/json'
-        },
-        body: JSON.stringify({
-            name: name,
-            price: price,
-            quantity_inventory: quantity_inventory,
-            quality: quality,
-            type: type,
-            description: description,
-            _token: token
-        })
-    })
-    .then(response => {
-        return response.json()
-    })
-    .then(result => {
         if (!result.errors) {
-            swal(
-                "Success!", 
-                'Produto cadastrado com sucesso!', 
-                "success"
-            );
-            
-            clearErrorsSpan(camposInputs);
-    
-            document.getElementById('name').value = ''
-            document.getElementById('price').value = ''
-            document.getElementById('quantity_inventory').value = ''
-            document.getElementById('quality').value = ''
-            document.getElementById('type').value = ''
-            document.getElementById('description').value = ''
-        }
-
-        document.getElementById("button").innerHTML = 'Adicionar';
-        document.getElementById("button").disabled = false;    
-
-        if (result.errors.name) {
-            camposInputs[0].innerHTML = result.errors.name;
+            clearInputs();
+            clearErrors();
+            swal("Success!", 'Produto cadastrado com sucesso!', "success");
         } else {
-            camposInputs[0].innerHTML = ''
+            showErrors(result.errors);
         }
 
-        if (result.errors.price) {
-            camposInputs[1].innerHTML = result.errors.price; 
-        } else {
-            camposInputs[1].innerHTML = ''
-        }
-
-        if (result.errors.quantity_inventory) {
-            camposInputs[2].innerHTML = result.errors.quantity_inventory;
-        } else {
-            camposInputs[2].innerHTML = ''
-        }
-
-        if (result.errors.quality) {
-            camposInputs[3].innerHTML = result.errors.quality;
-        } else {
-            camposInputs[3].innerHTML = ''
-        }
-
-        if (result.errors.type) {
-            camposInputs[4].innerHTML = result.errors.type; 
-        } else {
-            camposInputs[4].innerHTML = ''
-        }
-
-        if (result.errors.description) {
-            camposInputs[5].innerHTML = result.errors.description;
-        } else {
-            camposInputs[5].innerHTML = ''
-        }
-    })
-
-}); 
-
-const clearErrorsSpan = (param) => {
-    for(let i = 0; i < param.length; i++) {
-        let htmlError = param[i];    
-        htmlError.innerHTML = ''
+        submitButton.innerHTML = 'Adicionar';
+        submitButton.disabled = false;
     }
-}
+    catch (error) {
+        alert(error.message);
+    } finally {
+        submitButton.innerHTML = 'Adicionar';
+        submitButton.disabled = false;
+    }
+});
+
+const clearErrors = () => {
+  errorSpans.forEach(errorSpan => errorSpan.innerHTML = '');
+};
+
+const clearInputs = () => {
+  nameInput.value              = '';
+  priceInput.value             = '';
+  quantityInventoryInput.value = '';
+  qualityInput.value           = '';
+  typeInput.value              = '';
+  descriptionInput.value       = '';
+};
+
+const showErrors = (errors) => {
+  const errorKeys = ['name', 'price', 'quantity_inventory', 'quality', 'type', 'description'];
+  
+  errorKeys.forEach((errorKey, index) => {
+    if (errors[errorKey]) {
+        errorSpans[index].innerHTML = errors[errorKey];
+    }
+  });
+};
