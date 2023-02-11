3c698d08dba082130c34509a577f55089da6263f
Rafael Belchior <rafaelb89.rb@gmail.com>
2023-02-10 22:44:59 -0300

    tirando as responsabilidades do metodo store
    - um metodo para verificar quantidade do estoque, se for maior que 0, retorna true
    
    - um metodo para atualizar quantidade de produtos no banco de acordo com a compra

--- app/Http/Controllers/PurchasedController.php
+++ app/Http/Controllers/PurchasedController.php
@@ -3,56 +3,69 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use App\Models\PurchasedProducts;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PurchasedController extends Controller
 {
     public function store(string $idProduct)
     {
-        $product = Product::findOrFail($idProduct);
+
+        $product = Product::query()
+                    ->with(['user', 'shopping'])
+                    ->findOrFail($idProduct);
         
-        //valida e verifica se tem produto em estoque para comprar
-        if ($product->quantity_inventory == 0) {
+        if (!$this->checkInventory($product)) {
             return redirect()
-                    ->route('products.show', $idProduct)
-                    ->with('error', 'Sem produto no estoque para compra!');
+                        ->route('products.show', $idProduct)
+                        ->with('error', 'Sem produto no estoque para compra!');
         }
 
-        // verifica se esta "comprado"
-        if ($product->shopping) {
+        \DB::beginTransaction();
+    
+        try {
+            $result        = $this->toggleShopping($product);
+            $returnMessage = $result['message'];
+            $typeMessage   = $result['type'];
+        
+            $product->update();
+            \DB::commit();
             
-            $product->shopping->delete();
-            $product->quantity_inventory += 1;
-            
-            $product->save();
-            
+        } catch (\Exception $e) {
+            \DB::rollBack();
             return redirect()
-                    ->route('products.show', $idProduct)
-                    ->with('warning', 'Você não mais adquire esse produto!'); 
-        } else {
-            PurchasedProducts::create([
-                'user_id'    => auth()->user()->id,
-                'product_id' => $product->id
-            ]);
-
-            $product->quantity_inventory -= 1;
-            $product->save();
-
-            return redirect()
-                    ->route('products.show', $idProduct)
-                    ->with('success', 'Parabéns! Você adquiriu esse produto!'); 
+                ->route('products.show', $idProduct)
+                ->with('error', 'Ocorreu um erro ao atualizar o estoque.');
         }
+
+        return redirect()
+                    ->route('products.show', $idProduct)
+                    ->with($typeMessage, $returnMessage); 
     }
 
-    // essa funcao vai verificar se o produto esta ou nao como comprado
-    public function IsPurchased(string $id)
+    protected function checkInventory(Product $product): bool
     {
-
+        return $product->quantity_inventory > 0;
     }
 
+    protected function toggleShopping($product): array
+    {
+        if ($product->shopping) {
+            $product->shopping()->delete();
+            $product->quantity_inventory += 1;
 
+            $message = 'Você não mais adquire esse produto!';
+            $type    = 'warning';
+        } else {
+            $product->shopping()->create([
+                'user_id'    => $product->user->id,
+                'product_id' => $product->id,
+            ]);
+            $product->quantity_inventory -= 1;
+            
+            $message = 'Parabéns! Você adquiriu esse produto!';
+            $type    = 'success';
+        }
 
-
-
+        return ['message' => $message, 'type' => $type];
+    }
 }
