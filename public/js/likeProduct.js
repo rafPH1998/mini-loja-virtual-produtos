0f08bab1129527d2c4c472dab377e3a6adad013c
Rafael Belchior <rafaelb89.rb@gmail.com>
2023-02-21 15:58:39 -0300

    exibindo alerta caso seja curtindo ou descurtido o produto

--- public/js/likeProduct.js
+++ public/js/likeProduct.js
@@ -2,9 +2,11 @@ const likedPost = async (event, user, id, button) => {
 
     event.preventDefault(); 
 
+    button.innerHTML = '...'
+    button.disabled  = true;
+
     const url       = 'http://localhost:8989/products/like';
     const csrfToken = document.getElementById("_token").value;
-
 
     const config = {
         method: 'POST',
@@ -29,14 +31,18 @@ const likedPost = async (event, user, id, button) => {
         const isUserAction = responseJson.product_id === id && user === responseJson.user_id;
 
         if (responseJson.created === true && isUserAction) {
+
+            swal("Sucesso!", 'Você curtiu esse produto', 'success');
             button.innerHTML = `<button class="text-red-500 text-sm border-none focus:outline-none">descurtir</button>`;
+
         } else if (responseJson.deleted === true && isUserAction) {
+            swal("Aviso!", 'Você deixou de curtir o produto', 'warning');
             button.innerHTML = `<button class="text-green-500 text-sm border-none focus:outline-none">curtir</button>`;
         }
     } catch (error) {
         swal("Erro!", error, 'error');
     } finally {
-        button.disabled = false;
+       button.disabled = false;
     }
 }
 
