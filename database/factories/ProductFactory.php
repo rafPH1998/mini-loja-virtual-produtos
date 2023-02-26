807a3e400debf2e49a9c7ea53cffc5e22fd53dc2
Rafael Belchior <rafaelb89.rb@gmail.com>
2023-02-26 13:58:38 -0300

    atualizando o seed

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
@@ -18,7 +19,6 @@ class ProductFactory extends Factory
             'jogos', 
             'acessorios', 
             'brinquedos', 
-            'games', 
             'roupas', 
             'perfumaria'
         ];
@@ -30,9 +30,11 @@ class ProductFactory extends Factory
             'name'               => $this->faker->name(),
             'description'        => $this->faker->sentence(50),
             'type'               => $optionsType[0],
-            'image'              => 'https://source.unsplash.com/random',
+            'image'              => Storage::url('/images/images.jpeg'),
             'price'              => $this->faker->numberBetween(0, 500),
+            'discount'           => $this->faker->numberBetween(0, 500),
             'quantity_inventory' => $this->faker->numberBetween(0, 30),
+            'date'               => now()
         ];
     }
 }
