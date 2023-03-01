173281203cbb9e8b0be6e39cd662e20d0e8ccaf5
Rafael Belchior <rafaelb89.rb@gmail.com>
2023-03-01 15:57:20 -0300

    refatorando e colocando tirando arquivo js do blade

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,7 +1,9 @@
 <x-app>
     <div class="lg:w-4/5 mx-auto flex flex-wrap shadow-2xl">
-        <div class="xl:w-1/4 md:w-1/2 p-4">
-            sem imagem
+        <div class="xl:w-1/4 md:w-1/2">
+            <img class="h-full" 
+                src="{{ url("storage/{$product->image}") }}" alt="image"
+            />
         </div>
         <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0 ml-10">
             <h1 class="text-3xl title-font font-medium mb-1 text-white">{{ $product->name }}</h1>
@@ -94,36 +96,33 @@
         </div>
 
         @can('product-users', $product)
-            <form action="{{route('products.create_comment') }}" method="POST" class="mt-10">
+            <form action="#" method="POST" class="mt-10">
                 @csrf
-                @if (Session::has('comment_success'))
-                    <x-alerts.success>
-                        {{ Session::get('comment_success') }}
-                    </x-alerts.success>
-                @endif
-
+                <input type="hidden" name="_token" value="{{ csrf_token() }}" id="_token">
                 <input type="hidden" name="id" value="{{ $product->id }}">
                 <textarea 
-                    name="description" cols="30" rows="4" 
+                    name="description" id="description" cols="30" rows="4" 
                     class="w-full px-5 border-2
                     py-2 text-gray-700 bg-gray-200 
-                    focus:outline-none rounded @error('description') border-red-500 @enderror" 
+                    focus:outline-none rounded"
                     placeholder="O que achou do produto?"
                 ></textarea>
-                @error('description')
-                    @foreach ($errors->messages()['description'] as $error)
-                        <span class="text-red-500">{{ $error }}</span>
-                    @endforeach
-                @enderror
 
                 <div class="flex flex-row-reverse">
-                    <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 
+                    <button 
+                        id="buyProduct"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 
                         focus:ring-blue-300 font-medium rounded-lg 
                         text-sm px-5 py-2 dark:bg-blue-600 
                         dark:hover:bg-blue-700 focus:outline-none 
                         dark:focus:ring-blue-800 mt-1">
+                        <svg id="loading-button" style="display: none;" role="status" class="inline w-4 h-4 mr-3 text-white animate-spin" 
+                            viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+                        </svg>
                         Criar comentário
-                    </button>               
+                    </button>        
                 </div>
             </form> 
 
@@ -150,78 +149,6 @@
         @endcan
     </div>
 </x-app>
-
-
-<script>
-
-document.getElementById("submitButton").addEventListener("click", async function(event){
-
-    const submitButton  = document.getElementById('submitButton');
-    const csrfToken     = document.getElementById("_token").value;
-    const id            = document.getElementById("id").value;
-    const result_do_php = document.getElementById("result_do_php");
-    const result_do_js  = document.getElementById("result_do_js");
-    const result        = document.getElementById("result");
-    const qtdAtual      = document.getElementById("quantity_inventory").value;
-
-    //alert(qtdAtual)
-    
-    submitButton.innerHTML = 'Comprando...';
-    submitButton.disabled  = true;
-
-    event.preventDefault();
-
-    const url = `http://localhost:8989/products/buy`;
-
-    try {
-        const response = await fetch(url, {
-
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'accept': 'application/json'
-            },
-            body: JSON.stringify({
-                id    : id,
-                _token: csrfToken
-            })
-        });
-
-        const { quantity, countShopping, error, warning, success } = await response.json();
-        const buttonText = countShopping || error ? 'Comprar' : 'Remover compra';
-        submitButton.innerHTML = buttonText;
-
-        if (error) {
-            swal('Erro!', error, 'error');
-        } else if (warning) {
-            swal('Aviso!', warning, 'warning');
-            showButton(quantity);
-        } else {
-            result_do_php.style.display = 'none'
-            result_do_js.style.display = 'block'
-            showButton(quantity);
-            swal('Success!', success, 'success');
-        }
-
-    } catch (error) {
-        swal("Erro!", error, 'error');
-    } finally {
-        submitButton.disabled = false;
-    }
-})
-
-const showButton = (qtd) => {
-    if (qtd > 0) {
-        result.innerHTML = `(${qtd}) Em estoque`
-    } else { 
-        result.innerHTML = `(${qtd}) Estoque vazio`
-    }
-}
-</script>
-
-
-
-
 
 
 
