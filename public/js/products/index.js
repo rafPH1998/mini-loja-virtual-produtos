75eb2a45d5b105fc1671157bae7fb84466b29902
Rafael Belchior <rafaelb89.rb@gmail.com>
2023-03-12 14:55:56 -0300

    O que foi realizado:
    - exibindo links da paginação
    - modificando cor do link na paginação atual
    - limpando as sujeiras da paginação que volta do laravel

--- public/js/products/index.js
+++ public/js/products/index.js
@@ -1,4 +1,3 @@
-
 showPreloader();
 
 let currentPage = 1;
@@ -6,6 +5,8 @@ let currentPage = 1;
 // Função para buscar os dados de uma página usando AJAX
 const getPageData = async (page) => {
 
+    document.getElementById('resultLinks').innerHTML = ''
+    
     try {
         const url = `http://localhost:8989/products?page=${page}`;
         const response = await fetch(url);
@@ -16,6 +17,9 @@ const getPageData = async (page) => {
         } else {
             productNotFound()
         }
+
+        const cleanedLinks = cleanLinks(result.data.links);
+        showLinks(cleanedLinks)
       
         if (result.data.prev_page_url == null) {
             document.getElementById('previous-page').style.display = 'none';
@@ -37,15 +41,68 @@ const getPageData = async (page) => {
    
 };
 
+//Limpando os links, tirando aqueles icones sujos que voltam do laravel na paginação
+const cleanLinks = (links) => {
+    const cleanLinks = links.slice(); 
+    cleanLinks.shift(); 
+    cleanLinks.pop(); 
+    return cleanLinks;
+};
+
+// Funções para exibir todos os links da paginação
+const showLinks = (links) => {
+    
+    let html = '';
+    for (let i = 0; i < links.length; i++) {
+        
+        const link = links[i];
+        const id = i; // Define um ID exclusivo para cada link
+        const linkClass = link.active === true ? 'text-blue-700' : 'text-gray-500';
+        const bgClass   = link.active === true ? 'bg-blue-200' : 'bg-white';
+
+        html +=
+            `<a href="${link.url}" id="${id}" class="py-2 px-2 ml-0 
+                leading-tight border border-gray-300 
+                hover:bg-gray-100 hover:text-gray-700 text-xs
+                dark:bg-gray-800 dark:border-gray-700
+                dark:text-gray-400 dark:hover:bg-gray-700
+                dark:hover:text-white ${linkClass} ${bgClass}">
+                ${link.label}
+            </a>`;
+    }
+  
+    document.getElementById('resultLinks').innerHTML = html;
+  
+    // Adiciona o evento de clique a cada link individualmente
+    for (let i = 0; i < links.length; i++) {
+        const id = i
+        document.getElementById(id).addEventListener('click', (event) => {
+            event.preventDefault();
+            document.body.classList.add('loading');
+            showPreloader();
+
+            const url = new URL(event.target.getAttribute('href')); // Obtém a URL do link clicado
+            const page = url.searchParams.get('page');
+            getPageData(page)
+        });
+    }
+};
+
 // Funções para navegar para a próxima e anterior páginas
 document.getElementById('previous-page').addEventListener('click', (event) => {
     document.body.classList.add('loading');
     showPreloader();
     event.preventDefault();
-    if (currentPage > 1) {
+
+    if (currentPage === 0) {
+        currentPage = 1
+    }
+
+    if (currentPage >= 1) {
         currentPage--;
         getPageData(currentPage);
     }
+
 });
 
 document.getElementById('next-page').addEventListener('click', (event) => {
@@ -58,4 +115,3 @@ document.getElementById('next-page').addEventListener('click', (event) => {
 
 // Carrega os dados da primeira página ao carregar a página
 getPageData(currentPage);
-
